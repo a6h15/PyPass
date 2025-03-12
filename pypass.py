9fa961a472225f290db30d1cf99e0b1c8694be34
a6h1 <abhi.r1509.git@gmail.com>
2025-03-12 21:52:07 +0530

    Add files via upload

--- pypass.py
+++ pypass.py
@@ -5,7 +5,7 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-print(len(letters))
+
 print(f"---------------------------------------------------------------------------------------------------------------------------\n\n")
 
 print(r"""#  ____  _                                                                     _    ____                           _              
@@ -19,6 +19,22 @@ print(f"\n\n--------------------------------------------------------------------
 
 nr_letters = int(input("\t\tHow many letters would you like in your password?(a-z)(A-Z) , 52 letters -- \n\t\t"))
 nr_symbols = int(input(f"\t\tHow many symbols would you like?\n\t\t"))
-nr_numbers = int(input(f"\t\tHow many numbers would you like?\n\t\t"))
-random
+nr_numbers = int(input(f"\t\tHow many numbers would you like?(0-9)\n\t\t"))
+
+password = ""
+for i in range(0,nr_letters):
+    password += random.choice(letters)
+for i in range(0,nr_symbols):
+    password += random.choice(symbols)
+for i in range(0,nr_numbers):
+    password += random.choice(numbers)
+
+passlist = list(password)
+random.shuffle(passlist)
+
+jumbled = ''.join(passlist)
+print(f"Your randomly generated password is -- {jumbled}")
+
+
+
 
